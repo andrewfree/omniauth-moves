82dd604878dd0c95a28b3989d14b445e12143404
Andrew Free <andrew@schjelderup.org>
2014-03-07 15:25:40 -0800

    cleaning up output more to match standard.

--- lib/omniauth/strategies/moves.rb
+++ lib/omniauth/strategies/moves.rb
@@ -1,9 +1,8 @@
 require 'omniauth-oauth2'
-
 module OmniAuth
   module Strategies
     class Moves < OmniAuth::Strategies::OAuth2
-      DEFAULT_SCOPE = 'activity location'
+      option :name, "moves"
 
       option :client_options, {
         :site => 'https://api.moves-app.com/',
@@ -14,15 +13,6 @@ module OmniAuth
       uid { raw_info['userId'] }
       info do { :firstDate => (raw_info['profile'] || {})['firstDate'] } end
       extra do { :raw_info => raw_info } end
-
-      def request_phase
-        options[:authorize_params] = client_params.merge(options[:authorize_params])
-        super
-      end
-      
-      def auth_hash
-        OmniAuth::Utils.deep_merge(super, client_params.merge({:grant_type => 'authorization_code'}))
-      end
       
       def raw_info
         @raw_info ||= access_token.get('https://api.moves-app.com/api/v1/user/profile').parsed
@@ -36,3 +26,5 @@ module OmniAuth
     end
   end
 end
+
+OmniAuth.config.add_camelization 'moves', 'Moves'
